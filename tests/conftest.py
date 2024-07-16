98b48818bbda0b1c47d78e99cd29e20ae2ec7fa1
Anastasiia Strelnik <n.strelnik@mail.ru>
2024-07-16 18:42:59 +0200

    Добавлен размер окна браузера

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,12 @@
 import pytest
 from selene import browser
-from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_managment():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    #driver_options = webdriver.ChromeOptions()
-    #driver_options.add_argument('--headless') #в фоновом режиме
-    #browser.config.driver_options = driver_options
+def browser_management():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     yield
 
     browser.quit()
